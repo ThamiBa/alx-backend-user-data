19e74d8cde988b45ceba4f87eb454c0c7a8b7603
ThamiBa <baladithami@gmail.com>
2024-04-19 02:40:35 +0100

    fix_error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,3 +25,11 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """ GET /api/v1/unauthorized
+    Return:
+      - unauthorized error
+    """
+    abort(401)
+
