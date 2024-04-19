896ecf3c05aba8e932587c6020e844f5dae09ca0
ThamiBa <baladithami@gmail.com>
2024-04-19 02:59:12 +0100

    fix_error

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from typing import literal
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -21,7 +21,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
-def unauthorized(error) -> tuple[str, literal[401]]:
+def unauthorized(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Unauthorized"}), 401
